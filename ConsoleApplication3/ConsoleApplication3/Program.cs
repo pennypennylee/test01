ad35ed374ec32896c34734408bb21b388da17c5f
m305-36\w401 <123456@gmail.com>
2018-03-15 18:13:02 +0800

    老好玩喔

---
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("請輸入體重");
+            string kg = Console.ReadLine();
+            Console.WriteLine("請輸入身高");
+            string cm = Console.ReadLine();
+
+            double BMI = double.Parse(kg) / (double.Parse(cm) / 100 * double.Parse(cm) / 100);
+            Console.ReadLine();
+        }
+    }
+}
